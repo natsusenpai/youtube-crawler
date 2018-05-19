7d7e0939a5883b1c9d03c5403f02e3049d2cf220
natsusenpai <hatruongphu@gmail.com>
2018-05-19 21:49:59 +0700

    frontend 96%

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -7,34 +7,31 @@ use App\Channel;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
-{
-    
-    public function getDashboardSummary(Request $request) {
-        
-        $result = [];
+{   
+  public function getDashboardSummary(Request $request) {
+      
+    $result = [];
 
-        $startTime = $request->startTime ?? 0;
-            $endTime = $request->endTime ?? time();
-        $channelName = Channel::select('channel_name')
-            ->where('channel_id',$request->channelId)
-            ->get()->first()->channel_name;
-        
-        $chartData = YoutubeStatistics::select('total_videos', 'total_views', 'total_subcribers', 'total_likes', 'unixtime')
-            ->where('channel_id', $request->channelId)
-            ->whereBetween('unixtime', [$startTime, $endTime])
-            ->orderBy('id', 'desc')
-            ->get();
+    $startTime = $request->startTime ?? 0;
+      $endTime = $request->endTime ?? time();
+    $channelName = Channel::select('channel_name')
+      ->where('channel_id',$request->channelId)
+      ->get()->first()->channel_name;
+    
+    $chartData = YoutubeStatistics::select('total_videos as totalVideos', 'total_views as totalViews', 'total_subcribers as totalSubcribers', 'total_likes as totalLikes', 'unixtime')
+      ->where('channel_id', $request->channelId)
+      ->whereBetween('unixtime', [$startTime, $endTime])
+      ->orderBy('id', 'asc')
+      ->get();
 
-        $result = [
-            'channel_name' => $channelName,
-            'diff_video' => $chartData->first()->total_videos - $chartData->last()->total_videos,
-            'diff_view' => $chartData->first()->total_views - $chartData->last()->total_views,
-            'diff_sub' => $chartData->first()->total_subcribers - $chartData->last()->total_subcribers,
-            'diff_like' => $chartData->first()->total_likes - $chartData->last()->total_likes,
-            'chartData' => $chartData
-        ];
-        return $result;
-    }
-    
-    
+    $result = [
+      'channelName' => $channelName,
+      'diffVideo' => $chartData->last()->totalVideos - $chartData->first()->totalVideos,
+      'diffView' => $chartData->last()->totalViews - $chartData->first()->totalViews,
+      'diffSubcriber' => $chartData->last()->totalSubcribers - $chartData->first()->totalSubcribers,
+      'diffLike' => $chartData->last()->totalLikes - $chartData->first()->totalLikes,
+      'chartData' => $chartData
+    ];
+    return $result;
+  }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,79 +1,317 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
-    <script src="https://unpkg.com/axios@0.18.0/dist/axios.min.js"></script>
-    <div style="position: relative; height:15vh; width:80vw">
-        <label>Total Subcribers:</label>
-        <h1 id="numberOfView">Loading...</h1>
+  <!-- libraries -->
+  <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=" crossorigin="anonymous" />
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+  <script src="https://unpkg.com/axios@0.18.0/dist/axios.min.js"></script>
+  
+  <!-- actual layout -->
+  <div class="bg-light">
+    <div class="container rounded-0 bg-white">
+      <div class="row border-bottom"> 
+        <div class="col-1">
+          <div class="dropdown float-sm-right  d-inline-flex p-2">
+            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"></button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <a href="#" class="float-sm-left">
+            <img src="{{ asset('images/youtube_logo.png') }}" href="#" class="img-fluid  d-inline-flex p-2">
+          </a>
+        </div>
+        <div class="col">
+          <div class="float-sm-right d-inline-flex p-2">
+            <button href="#" class="btn btn-light border rounded col">
+              View detail
+            </button>
+            <a href="#">
+              <img src="{{ asset('images/link-chain.png') }}" class="img-fluid p-2" style="max-height:40px;" >
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        
+        <div class="col-2 border-right d-flex align-items-center justify-content-center">
+          <div>  
+            <span class="p-2 text-secondary">Total Videos</span><br>
+            <h1 id="numberVideos" class="p-2"></h1>
+          </div>
+          
+        </div>
+
+        <div class="col">
+          <div class="row">
+
+            <div class="col">
+              <div class="chart-container">
+                <div class="row">
+                  <span class="text-secondary">Total Views</span>
+                </div>
+                <div class="row">
+                  <h1 id="numberViews" class="float-sm-left text-secondary">
+                    loading...
+                  </h1>
+                  <!-- <span class="oi oi-arrow-bottom"></span> -->
+                  <span id="diffView" class="p-2">
+                    loading...
+                  </span>
+                  
+                </div>
+                <canvas id="viewChart"></canvas>
+              </div>              
+            </div>
+
+            <div class="col">
+              <div class="chart-container" >
+                <div class="row">
+                  <span class="text-secondary">Total Likes</span>
+                </div>
+                <div class="row">
+                  <h1 id="numberLikes" class="float-sm-left text-secondary">
+                    loading...
+                  </h1>
+                  <span id="diffLike" class="p-2">
+                    loading...
+                  </span>
+                </div>
+                <canvas id="likeChart"></canvas>
+              </div>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+              <div class="chart-container" >
+                <div class="row">
+                  <span class="text-secondary">Total Subcribers</span>
+                </div>
+                <div class="row">
+                  <h1 id="numberSubcribers" class="float-sm-left text-secondary">
+                    loading...
+                  </h1>
+                  <span id="diffSubcriber" class="p-2">
+                    loading...
+                  </span>
+                </div>
+                <canvas id="subcriberChart"></canvas>
+              </div>
+            </div>
+            <div class="col"></div>
+          </div>
+        </div>
+      </div> 
     </div>
-    <div class="chart-container" style="position: relative; height:40vh; width:80vw">
-        <canvas id="myChart"></canvas>
-    </div>
+
+  </div>
     
-    <script>
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var data = [], labels = [];
-    
+  <!-- chartjs rendering -->
+  <script>
+    let subcriberChartData = {},
+        viewChartData = {},
+        likeChartData = {};
 
     axios.get('/dashboard?channelId=UCSJ4gkVC6NrvII8umztf0Ow')
-        .then(function (response) {
-            document.getElementById("numberOfView").innerHTML = response.data.chartData[0].total_subcribers;
-            response.data.chartData.forEach((item)=>{
-                var date = new Date(item.unixtime*1000);
-                data.push(item.total_subcribers);
-                labels.push(`Time: ${date}`);    
-            })
-            
-            var purple_orange_gradient = ctx.createLinearGradient(0, 0, 0, 600);
-            purple_orange_gradient.addColorStop(0, 'rgba(255, 227, 166, 1)');
-            purple_orange_gradient.addColorStop(1, 'rgba(255, 242, 213, 1)');
+    .then(function (response) {
+      $('#numberVideos').html(convertNumber(response.data.chartData[0].totalVideos));
+      $('#numberViews').html(convertNumber(response.data.chartData[0].totalViews));
+      $('#numberSubcribers').html(convertNumber(response.data.chartData[0].totalSubcribers));
+      $('#numberLikes').html(convertNumber(response.data.chartData[0].totalLikes));
+      $('#diffSubcriber').html(convertPercent(response.data.chartData[0].totalSubcribers, response.data.diffSubcriber));
+      $('#diffLike').html(convertNumber(response.data.diffLike));
+      $('#diffView').html(convertNumber(response.data.diffView));
 
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: labels.reverse(),
-                    datasets: [{
-                        // fillColor : gradient, // Put the gradient here as a fill color
-                        // strokeColor : "#ff6c23",
-                        // pointColor : "#fff",
-                        // pointStrokeColor : "#ff6c23",
-                        // pointHighlightFill: "#fff",
-                        // pointHighlightStroke: "#ff6c23",
-                        label: '# of subscriber',
-                        data: data.reverse(),
-                        backgroundColor: purple_orange_gradient,
-                        borderColor: [
-                            'rgba(255, 204, 94, 1)',
-                            'rgba(255, 204, 94, 1)',
-                            'rgba(255, 204, 94, 1)',
-                        ],
-                        borderWidth: 3
-                    }]
-                },
-                options: {
-                    scales: {
-                        xAxes: [{
-                            display: false
-                        }],
-                        yAxes: [{
-                            display: false,
-                            ticks: {
-                                beginAtZero: false
-                            }
-                        }]
-                    }
-                }
-            });
-            
-            
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+      timestampLabels = response.data.chartData.map(item => `Time: ${new Date(item.unixtime*1000)}`);
+      subcriberChartData.labels = timestampLabels;
+      viewChartData.labels = timestampLabels;
+      likeChartData.labels = timestampLabels;
+      subcriberChartData.data = response.data.chartData.map(item => item.totalSubcribers);
+      viewChartData.data = response.data.chartData.map(item => item.totalViews);
+      likeChartData.data = response.data.chartData.map(item => item.totalLabels);
     
-    </script>
+      renderSubcriberChart("subcriberChart", subcriberChartData);
+      renderViewChart("viewChart", viewChartData);
+      renderLikeChart("likeChart", subcriberChartData);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+      
+    function renderSubcriberChart(documentId, data) {
+      let ctx = document.getElementById(documentId).getContext('2d');
+      let orange_white_gradient = ctx.createLinearGradient(0, 0, 0, 600);
+      orange_white_gradient.addColorStop(0, 'rgba(255, 227, 166, 1)');
+      orange_white_gradient.addColorStop(1, 'rgba(255, 255, 255, 0)');
+
+      let subcriberChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.labels,
+          datasets: [{
+            // label: '# of subcriber',
+            data: data.data,
+            backgroundColor: orange_white_gradient,
+            borderColor: [
+              'rgba(255, 204, 94, 1)'
+            ],
+            borderWidth: 2
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem) {
+                return tooltipItem.yLabel;
+              }
+            }
+          },
+          scales: {
+            xAxes: [{
+              display: false
+            }],
+            yAxes: [{
+              display: false,
+              ticks: {
+                  beginAtZero: false,
+                  maxTicksLimit:2
+              }
+            }]
+          }
+        }
+      });   
+    }
+
+    function renderViewChart(documentId, data) {
+      let ctx = document.getElementById(documentId).getContext('2d');
+      let orange_white_gradient = ctx.createLinearGradient(0, 0, 0, 600);
+      orange_white_gradient.addColorStop(0, 'rgba(255, 227, 166, 1)');
+      orange_white_gradient.addColorStop(1, 'rgba(255, 255, 255, 0)');
+
+      let subcriberChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.labels,
+          datasets: [{
+            // label: '# of subcriber',
+            data: data.data,
+            backgroundColor: orange_white_gradient,
+            borderColor: [
+              'rgba(255, 204, 94, 1)'
+            ],
+            borderWidth: 2
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem) {
+                return tooltipItem.yLabel;
+              }
+            }
+          },
+          scales: {
+            xAxes: [{
+              display: false
+            }],
+            yAxes: [{
+              display: false,
+              ticks: {
+                  beginAtZero: false,
+                  maxTicksLimit:2
+              }
+            }]
+          }
+        }
+      });   
+    }
+
+    function renderLikeChart(documentId, data) {
+      let ctx = document.getElementById(documentId).getContext('2d');
+      let orange_white_gradient = ctx.createLinearGradient(0, 0, 0, 600);
+      orange_white_gradient.addColorStop(0, 'rgba(255, 227, 166, 1)');
+      orange_white_gradient.addColorStop(1, 'rgba(255, 255, 255, 0)');
+
+      let subcriberChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.labels,
+          datasets: [{
+            // label: '# of subcriber',
+            data: data.data,
+            backgroundColor: orange_white_gradient,
+            borderColor: [
+              'rgba(255, 204, 94, 1)'
+            ],
+            borderWidth: 2
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem) {
+                return tooltipItem.yLabel;
+              }
+            }
+          },
+          scales: {
+            xAxes: [{
+              display: false
+            }],
+            yAxes: [{
+              display: false,
+              ticks: {
+                  beginAtZero: false,
+                  maxTicksLimit:2
+              }
+            }]
+          }
+        }
+      });   
+    }
+    
+    function convertNumber(number, unit=1000000000) {
+      if (number == 0) return number;
+      const dict = {
+        "1" : "",
+        "1000" : "K",
+        "1000000" : "M",
+        "1000000000" : "B",
+      };
+      roundDiv = Math.floor(Math.abs(number)/unit);
+      if (roundDiv) {
+        return `${roundDiv}${dict[unit]}`;
+      } 
+      else {
+        return convertNumber(number, Math.floor(unit/1000));
+      }
+    }
+
+    function convertPercent(total, diff) {
+      if (total == diff) return 0;
+      return `${(diff*100/total).toFixed(2)}%`;
+    }
+
+      
+  </script>
 </html>
